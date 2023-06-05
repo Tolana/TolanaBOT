5f84abb5034fcac8342aa641eb397a75a7030da4
= <=>
2023-06-06 01:09:08 +0200

    refactor to mysql 1.0

--- scrape/adbl.py
+++ scrape/adbl.py
@@ -7,6 +7,7 @@ sys.path.append(str(pcwd))
 import requests
 from bs4 import BeautifulSoup
 from my_db.db import DBManager
+from my_db.db import open_conn
 from time import sleep
 import sqlite3
 import mysql.connector
@@ -19,6 +20,7 @@ headers = {
     with open(filename,'rb') as f:
         return f.read()"""
 
+con, cur = open_conn()
 
 def scrape(url,page=None):
     url = url + "?pageSize=50"
@@ -81,71 +83,89 @@ def getSeriesPages(soup):
 
 
 # DB FUNCTIONS :)
-
+#with DBManager() as cur:
 def checkForNewBook(url,count):
-    with DBManager('example.db') as cur:
+    #with DBManager('example.db') as cur:
+    cur.execute(f'SELECT `books` FROM `seriesCount` WHERE `series_url` = "{url}"')
+    #print(f'SELECT `books` FROM `seriesCount` WHERE `series_url` = "{url}"')
+    book_count = cur.fetchone()
+    if book_count is not None:
+        book_count = book_count[0]
+    print("BOOK_COUNT: ",book_count)
+    if book_count is None:
+        print('was none!')
+        cur.execute(f'REPLACE INTO `seriesCount` VALUES("{url}","{count}")')
+        con.commit()
+        return 0
+    else: 
         cur.execute(f'SELECT `books` FROM `seriesCount` WHERE `series_url` = "{url}"')
-        #print(f'SELECT `books` FROM `seriesCount` WHERE `series_url` = "{url}"')
-        book_count = cur.fetchone()[0]
-        print("BOOK_COUNT: ",book_count)
-        if book_count is None:
-            print('was none!')
-            cur.execute(f'REPLACE INTO seriesCount VALUES("{url}","{count}")')
-            return 0
-        else: 
-            cur.execute(f'SELECT `books` FROM seriesCount WHERE `series_url` = "{url}"')
-            old_count = cur.fetchone()[0]
-            print(old_count)
-            cur.execute(f'REPLACE INTO seriesCount VALUES("{url}","{count}")')
-            return old_count
+        old_count = cur.fetchone()[0]
+        print(old_count)
+        cur.execute(f'REPLACE INTO `seriesCount` VALUES("{url}","{count}")')
+        con.commit()
+        return old_count
             
 
 def getUrls(): 
-    with DBManager('example.db') as cur:
-        cur.execute('SELECT url FROM url') 
-        urls = [row[0] for row in cur]
-        return urls
+    #with DBManager('example.db') as cur:
+    cur.execute('SELECT url FROM url') 
+    urls = [row[0] for row in cur]
+    return urls
 
 def insertSeries(url,title,authors):
+    print("Insert Series")
     rowid = ""
     for author in authors:
         insertAuthor(author[0],author[1])
-    with DBManager('example.db') as cur:
-        try:
-            #print('InsertSeries: '+ f'INSERT INTO series ("title","series_url") VALUES("{title}","{url}");')
-            cur.execute(f'INSERT INTO `series` (`title`,`series_url`) VALUES("{title}","{url}");')
-            rowid = cur.lastrowid
-        except sqlite3.IntegrityError:
-            '''cur.execute(f'SELECT id FROM `series` WHERE `series.series_url` = "{url}"')
-            for row in cur:
-                rowid = row[0]'''
-            return
+    #with DBManager('example.db') as cur:
+    try:
+        #print('InsertSeries: '+ f'INSERT INTO series ("title","series_url") VALUES("{title}","{url}");')
+        cur.execute(f'INSERT INTO `series` (`title`,`series_url`) VALUES("{title}","{url}");')
+        rowid = cur.lastrowid
+    except sqlite3.IntegrityError:
+        '''cur.execute(f'SELECT id FROM `series` WHERE `series.series_url` = "{url}"')
+        for row in cur:
+            rowid = row[0]'''
+        return
+    else:
+        print("Commit!")
+        con.commit()
     for author in authors:
         insertSeriesAuthor(author[1],rowid)
+    print("end of insert series")
     return
 
 def insertSeriesAuthor(url,id):
-    with DBManager('example.db') as cur:
-        try:
-            cur.execute(f'INSERT INTO `seriesAuthors` VALUES("{url}","{id}")')
-        except mysql.connector.IntegrityError:
-            pass
+    #with DBManager('example.db') as cur:
+    try:
+        cur.execute(f'INSERT INTO `seriesAuthors` VALUES("{url}","{id}")')
+    except mysql.connector.IntegrityError:
+        pass
+    else:
+        print("Commit!")
+        con.commit()
     return
 
 def insertAuthor(name,url):
-    with DBManager('example.db') as cur:
-        try:
-            cur.execute(f'INSERT INTO `author` VALUES("{name}","{url}")')
-        except mysql.connector.IntegrityError:
-            pass
+    #with DBManager('example.db') as cur:
+    try:
+        cur.execute(f'INSERT INTO `author` VALUES("{name}","{url}")')
+    except mysql.connector.IntegrityError:
+        pass
+    else:
+        print("Commit!")
+        con.commit()
     return
 
 def insertNarrator(name,url):
-    with DBManager('example.db') as cur:
-        try:
-            cur.execute(f'INSERT INTO `narrator` VALUES("{name}","{url}")')
-        except mysql.connector.IntegrityError:
-            pass
+    #with DBManager('example.db') as cur:
+    try:
+        cur.execute(f'INSERT INTO `narrator` VALUES("{name}","{url}")')
+    except mysql.connector.IntegrityError:
+        pass
+    else:
+        print("Commit!")
+        con.commit()
     return
 
 def insertBook(book,url):
@@ -153,36 +173,46 @@ def insertBook(book,url):
     title = book['title']
     length = book['length']
     releaseDate = book['release_date']
-    with DBManager('example.db') as cur:
-        try:
-            cur.execute(f'INSERT INTO book (`title`,`series_url`,`length`,`releaseDate`) VALUES("{title}","{url}","{length}","{releaseDate}")')
-            rowid = cur.lastrowid
-            cur.execute(f'INSERT INTO bookQ VALUES({rowid});')
-        except mysql.connector.IntegrityError:
-            return
+    #with DBManager('example.db') as cur:
+    try:
+        cur.execute(f'INSERT INTO book (`title`,`series_url`,`length`,`releaseDate`) VALUES("{title}","{url}","{length}","{releaseDate}")')
+        rowid = cur.lastrowid
+        cur.execute(f'INSERT INTO bookQ VALUES({rowid});')
+    except mysql.connector.IntegrityError:
+        return
+    else:
+        print("Commit!")
+        con.commit()
     return rowid
 
 def insertBookAuthors(authors,id):
     for author in authors:
         url = author[1]
-        with DBManager('example.db') as cur:
-            try:
-                cur.execute(f'INSERT INTO `authors` VALUES("{url}","{id}")')
-            except mysql.connector.IntegrityError:
-                pass
+        #with DBManager('example.db') as cur:
+        try:
+            cur.execute(f'INSERT INTO `authors` VALUES("{url}","{id}")')
+        except mysql.connector.IntegrityError:
+            pass
+        else:
+            print("Commit!")
+            con.commit()
     return
 
 def insertBookNarrators(narrators,id):
     for narrator in narrators:
         url = narrator[1]
-        with DBManager('example.db') as cur:
-            try:
-                cur.execute(f'INSERT INTO `narrators` VALUES("{url}","{id}")')
-            except mysql.connector.IntegrityError:
-                pass
+        #with DBManager('example.db') as cur:
+        try:
+            cur.execute(f'INSERT INTO `narrators` VALUES("{url}","{id}")')
+        except mysql.connector.IntegrityError:
+            pass
+        else:
+            print("commit!")
+            con.commit()
     return
 
 def insertBooks(books,url):
+    print("Insert books ")
     for book in books:
         if book is not None:
             for author in book['authors']:
@@ -195,6 +225,7 @@ def insertBooks(books,url):
                 insertBookNarrators(book['narrators'],id)
             else:
                 pass
+    print("end of insert books")
     return
 
 # END OF DB FUNCTIONS
@@ -206,9 +237,12 @@ def run():
     for url in urls:
         print('waiting on delay...')
         sleep(_DELAY)
+        print('end of delay')
         html = scrape(url)
         soup = BeautifulSoup(html, 'html.parser')
+        print("Got soup")
         pages = getSeriesPages(soup)
+        print('got pages')
         if len(pages) != 0:
             pages = pages[1:]
             books = saveBooks(soup)
@@ -230,10 +264,14 @@ def run():
     print(f'Completed in: {end_time}')
     return
 run()
-with DBManager('example.db') as cur:
-    cur.execute('SELECT * FROM bookQ') 
-    count = len(cur.fetchall())
-    if count > 100:
-        cur.execute('DELETE FROM bookQ;')
-    else: 
-        pass
+
+
+cur.execute('SELECT * FROM bookQ') 
+count = len(cur.fetchall())
+if count > 100:
+    cur.execute('DELETE FROM bookQ;')
+else: 
+    pass
+
+con.commit()
+con.close()
