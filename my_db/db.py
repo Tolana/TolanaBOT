f64f27092ac86ffc5802c78fcd9a8789c06e226b
= <=>
2023-06-06 01:08:53 +0200

    DBManager replaced with open conn, slow with MySQL

--- my_db/db.py
+++ my_db/db.py
@@ -1,21 +1,19 @@
 import sqlite3 
 import mysql.connector
 
-def open_conn(dbname):
-    con = mysql.connector.connect(host="localhost",user="admin",password="")
+def open_conn(dbname=""):
+    con = mysql.connector.connect(host="localhost",user="root",password="",database="audiblebooks")
     cur = con.cursor()
     return con, cur
 
 class DBManager(object):
-    def __init__(self, dbname):
+    def __init__(self, dbname=""):
         self.db = None
         self.dbname = dbname
 
     def __enter__(self):
-        self.db = mysql.connector.connect(host="localhost",user="root",password="",database="audiblebooks")
-        return self.db.cursor()
+        return cur
 
     def __exit__(self, type, value, traceback):
-        self.db.commit()
-        self.db.close()
+        print(con.commit())
         #print('closed db connection')
